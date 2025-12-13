1337d7ee6f633ffc5ddac71cc35f8d84fdd55590
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-12-13 18:01:23 +0545

    changes l to L

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import './LoginPopup.css'
+import './loginPopup.css'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
