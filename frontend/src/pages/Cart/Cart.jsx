42715b1b18fe70ce22cc85191d1e21bc26a1610b
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-12-18 09:13:18 +0545

    the issue in cart without user login is solved

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,30 @@
 import React, { useContext } from 'react'
 import './Cart.css'
-import { StoreContext } from '../../context/StoreContext';
-import { useNavigate } from 'react-router-dom';
-const Cart = () => {
+import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
-  const { cartItems, food_list, removeFromCart,getTotalCartAmount,url } = useContext(StoreContext);
+const Cart = ({setShowLogin}) => {
+  const {
+    cartItems,
+    food_list,
+    removeFromCart,
+    getTotalCartAmount,
+    url
+    
+  } = useContext(StoreContext)
 
-  const navigate =useNavigate();
+  const navigate = useNavigate()
+
+  const handleCheckout = () => {
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+      setShowLogin(true)
+      return
+    }
+
+    navigate('/order')
+  }
 
   return (
     <div className='cart'>
@@ -21,12 +39,13 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item, index) => {
+
+        {food_list.map((item) => {
           if (cartItems[item._id] > 0) {
             return (
               <div key={item._id}>
-                <div className="cart-items-title  cart-items-item">
-                  <img src={url+"/images/"+item.image} alt="" />
+                <div className="cart-items-title cart-items-item">
+                  <img src={url + "/images/" + item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
@@ -37,41 +56,45 @@ const Cart = () => {
               </div>
             )
           }
-
+          return null
         })}
       </div>
+
       <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Totals</h2>
-          <div>
-            <div className="cart-total-details">
-              <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
-            </div>
-            <hr />
-            <div className="cart-total-details">
-              <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
-            </div>
-            <hr />
-            <div className="cart-total-details">
-              <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
-            </div>
 
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>${getTotalCartAmount()}</p>
           </div>
-          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+          <hr />
+
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+          </div>
+          <hr />
+
+          <div className="cart-total-details">
+            <b>Total</b>
+            <b>
+              ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+            </b>
+          </div>
+
+          <button onClick={handleCheckout}>
+            PROCEED TO CHECKOUT
+          </button>
         </div>
+
         <div className="cart-promocode">
-          <div>
-            <p>If you have a promo code ,  Enter it here</p>
-            <div className="cart-promocode-input">
-              <input type="text"  placeholder='promo code'/>
-              <button>Submit</button>
-            </div>
+          <p>If you have a promo code, enter it here</p>
+          <div className="cart-promocode-input">
+            <input type="text" placeholder='promo code' />
+            <button>Submit</button>
           </div>
         </div>
-
       </div>
     </div>
   )
